0fb43bbf818d5f9d94494b4de9a747478105b96a
Ian Wu <54074286+ianwu13@users.noreply.github.com>
2025-05-13 20:13:18 -0700

    Delete backend/sales_thread directory

--- backend/sales_thread/lead_target.py
+++
@@ -1,7 +0,0 @@
-class LeadTarget:
-    def __init__(self):
-        self.lead_id = None
-        self.lead_name = None
-        self.lead_email = None
-        self.lead_phone = None
-        self.lead_company = None
